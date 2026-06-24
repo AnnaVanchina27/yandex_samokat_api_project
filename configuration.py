7ab922128f197d8c86cd535086f726012980a797
AnnaVanchina27 <Annavanchina@yandex.ru>
2026-06-24 15:09:41 +0300

    Add SQL query and autotest

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://cb4f1473-e963-4f17-849f-199a61d340d8.serverhub.praktikum-services.ru"
+
+
+CREATE_ORDERS_PATH = "/api/v1/orders"
+GET_ORDERS_PATH = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+orders_body = {
+    "firstName": "Мила",
+    "lastName": "Йовович",
+    "address": "Проспект Мира, 44-12",
+    "metroStation": 89,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-24",
+    "comment": "Оставьте у двери",
+    "color": [
+        "BLACK"
+    ]
+}
